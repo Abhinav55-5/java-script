10e6a98a0c3258848def5b29b6ce885518865a6a
Abhinav55-5 <abhinavsingh6979@gmail.com>
2026-06-28 12:37:46 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"  //treat all JS code as a newer version 
+
+// alert(3+3) we are using nodejs not browser 
+
+let name="abhinav" // string
+let age=18 // number 
+let loggerIn=true; // boolean
+
+// number => 2 power 53
+// bigint 
+// string =""
+// bollean =true/false
+// undefined 
+// null 
+
+// object 
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined 
