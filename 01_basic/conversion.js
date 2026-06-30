c5fdfd22b0e9d01b4f2399135fa84830647cbe23
Abhinav55-5 <abhinavsingh6979@gmail.com>
2026-06-30 17:29:07 +0000

    conversion

---
+++ 01_basic/conversion.js
@@ -0,0 +1,27 @@
+// let score="33abd";
+// let score=null;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueinnumber=Number(score)
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
+
+// "33"=33
+// "33abd"=nan
+// true=>1,false=0;
+
+// let loggedIn="";
+// let logIN=Boolean(loggedIn);
+// console.log(logIN);
+
+// ""=false
+// "abhinav"=true
+
+let string="33";
+let change=String(string);
+console.log(typeof change)
+console.log(change);
+;
+//  ******************operations ************
