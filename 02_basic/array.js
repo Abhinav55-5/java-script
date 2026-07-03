2f182affe259885b82d67008dc8c392ef0a24918
Abhinav55-5 <abhinavsingh6979@gmail.com>
2026-07-03 16:26:32 +0000

    array

---
+++ 02_basic/array.js
@@ -0,0 +1,19 @@
+// arrays 
+const Myarr=[1,2,3,4,5];
+const superhero=["shaktiman","spiderman"]
+
+console.log(Myarr[0]);
+
+Myarr.push(6);
+Myarr.pop();
+
+// Myarr.unshift(6)  //add the numbers at the start of the array 
+// Myarr.shift();    // remove the numbers from the front
+
+console.log(Myarr);
+console.log(Myarr.includes(9))
+console.log(Myarr.indexOf(3))
+
+const newarr=Myarr.join()
+
+
