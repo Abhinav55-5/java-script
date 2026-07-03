ebe7ac291321766c516769ca200a42626994e0c3
Abhinav55-5 <abhinavsingh6979@gmail.com>
2026-07-03 19:13:47 +0000

    array2

--- 02_basic/array.js
+++ 02_basic/array.js
@@ -1,8 +1,9 @@
 // arrays 
+
 const Myarr=[1,2,3,4,5];
 const superhero=["shaktiman","spiderman"]
 
-console.log(Myarr[0]);
+// console.log(Myarr[0]);
 
 Myarr.push(6);
 Myarr.pop();
@@ -10,10 +11,31 @@ Myarr.pop();
 // Myarr.unshift(6)  //add the numbers at the start of the array 
 // Myarr.shift();    // remove the numbers from the front
 
-console.log(Myarr);
-console.log(Myarr.includes(9))
-console.log(Myarr.indexOf(3))
+// console.log(Myarr);
+// console.log(Myarr.includes(9))
+// console.log(Myarr.indexOf(3))
+
 
 const newarr=Myarr.join()
+// change the array into string and add into newarr
+// console.log(Myarr);
+// console.log(newarr);
+// console.log(typeof newarr)
 
+// slice splice
 
+// In case of slice there is no chane in the array
+
+console.log("A ", Myarr);
+
+const myn1 = Myarr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", Myarr);
+// In the case of splice there is change in the original array 
+// and also includes the last element of the range 
+
+const myn2 = Myarr.splice(1, 3)
+
+console.log(myn2);
+console.log("C ", Myarr);
